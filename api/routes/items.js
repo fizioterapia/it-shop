ce18f577a76903b9132fc050cabe7f29fdd51c5d
fizioterapia <cubasa4@gmail.com>
2022-07-03 01:09:59 +0200

    add featured items

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,16 +1,18 @@
-const { log } = require('console');
 const express = require('express');
-const { send } = require('process');
 const router = express.Router()
 
-router.get('/:itemID', async (req, res) => {
+router.get('/featured', async (req, res) => {
+    console.log('a')
     try {
-        if (Number.isNaN(req.params.itemID)) throw "Invalid number";
+        let rows = await req.db.query("SELECT products.* from `products` WHERE sold > 0 ORDER BY sold DESC LIMIT 6");
 
-        let rows = await req.db.query("SELECT products.*, categories.name as categoryName from products INNER JOIN categories ON products.categoryId = categories.id WHERE products.id = ?", req.params.itemID)
+        if (rows.length <= 0) {
+            rows = await reg.db.query("SELECT products.* from `products` ORDER by price DESC LIMIT 6");
+        }
+
         res.json(rows);
-    } catch (e) {
-        res.json({error: "Not a number!"});
+    } catch(e) {
+        res.json({error: e})
     }
 });
 
@@ -41,6 +43,17 @@ router.post('/add', async (req, res) => {
     } catch(e) {
         res.json({error: "Contact administrator."})
         console.error(e);
+    }
+});
+
+router.get('/:itemID', async (req, res) => {
+    try {
+        if (Number.isNaN(req.params.itemID)) throw "Invalid number";
+
+        let rows = await req.db.query("SELECT products.*, categories.name as categoryName from products INNER JOIN categories ON products.categoryId = categories.id WHERE products.id = ?", req.params.itemID)
+        res.json(rows);
+    } catch (e) {
+        res.json({error: e});
     }
 });
 
